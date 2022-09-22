294bd3c09b7bdac01bce8a2c916f0588816469ae
Byron Alfredo Barriga Rivera <administradormo01@plasticaucho.com>
2022-09-22 15:31:33 -0500

    PEDSUG V2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
  // apiUrl: 'http://outgoapp.plasticaucho.com/WSAtiendo/api/'
-  apiUrl: 'https://10.11.2.172:45455/api/'
+  apiUrl: 'https://10.11.2.172:45456/api/'
   //apiUrl: 'http://192.168.100.174:45455/api/'
 
 };
