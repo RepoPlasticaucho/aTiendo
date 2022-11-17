6f9b254897fd27d16397e95c076c112c72804e69
Byron Alfredo Barriga Rivera <administradormo01@plasticaucho.com>
2022-11-17 16:58:59 -0500

    ACT PER 2

--- src/app/components/users/usuario-pass/usuario-pass.component.ts
+++ src/app/components/users/usuario-pass/usuario-pass.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faCopy, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { SociedadesService } from 'src/app/services/sociedades.service';
 
 @Component({
   selector: 'app-usuario-pass',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-pass.component.css']
 })
 export class UsuarioPassComponent implements OnInit {
-
-  constructor() { }
+  faTimes = faTimes;
+  faCopy = faCopy;
+  faSave = faSave;
+  //Creación de la variable para formulario
+  corporationForm = new FormGroup({
+    idFiscal: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    nombreComercial: new FormControl('', Validators.required),
+    razonSocial: new FormControl('', Validators.required),
+    correoElectronico: new FormControl('', [Validators.required, Validators.email]),
+    telefono: new FormControl('', [Validators.required, Validators.minLength(9)]),
+  });
+  constructor( private readonly httpService: SociedadesService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onPass(): void{
+    
+    this.router.navigate(['/navegation-adm', { outlets: { 'contentAdmin': ['usuario'] } }]);
+  }
+  onSubmit(){
+
+  }
 }
