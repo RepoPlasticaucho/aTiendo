737a27bbfa1188f768dcd231c59b5bb0a6c2ff8a
Byron Alfredo Barriga Rivera <administradormo01@plasticaucho.com>
2022-12-06 14:30:28 -0500

    CAT MAS V 2

--- src/app/components/inventories/catalogos/catalogos.component.ts
+++ src/app/components/inventories/catalogos/catalogos.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faEdit, faList, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Subject } from 'rxjs';
+import { AtributosEntity } from 'src/app/models/atributos';
 import { CatalogosEntity } from 'src/app/models/catalogos';
 import { CategoriasEntity } from 'src/app/models/categorias';
+import { ColorsEntity } from 'src/app/models/colors';
+import { GenerosEntity } from 'src/app/models/generos';
+import { MarcasEntity } from 'src/app/models/marcas';
 import { CatalogosService } from 'src/app/services/catalogos.service';
 import { CategoriasService } from 'src/app/services/categorias.service';
 import Swal from 'sweetalert2';
@@ -49,20 +53,22 @@ export class CatalogosComponent implements OnInit {
           showConfirmButton: false,
         });
       } else {
-        this.lstCatalogos = res.lstCatalogos;
-        this.dtTrigger.next('');
-       // console.log(res); 
-        const catalogoCategorias = [...new Set(res.lstCatalogos.map(item => item.categoria))];
-     
-        catalogoCategorias.forEach((value) =>{
-          const categoria : CategoriasEntity ={
-            id: '',
-            categoria: value,
-            cod_sap: '',
-            etiquetas: '',
-            almacen_id: '',
-          }   
-           // console.log(categoria);
+          this.lstCatalogos = res.lstCatalogos;
+          this.dtTrigger.next('');
+          // console.log(res); 
+          
+          //CARGA DE CATEGORIAS.
+          const catalogoCategorias = [...new Set(res.lstCatalogos.map(item => item.categoria))];
+          catalogoCategorias.forEach((value) =>{
+            
+            const categoria : CategoriasEntity ={
+              id: '',
+              categoria: value,
+              cod_sap: '',
+              etiquetas: '',
+              almacen_id: '',
+            }   
+            // console.log(categoria);
             this.httpService.obtenerCategoriasNombre(categoria).subscribe(res =>{
               if (res.codigoError != "OK") {
                 const categorianew : CategoriasEntity ={
@@ -74,7 +80,7 @@ export class CatalogosComponent implements OnInit {
                 }
                 this.httpService.agregarCategoria(categorianew).subscribe(res =>{
                   if (res.codigoError == "OK") {
-                    
+                    console.log("carga de Categorias con exito ")
                   }else{
                     Swal.fire({
                       icon: 'error',
@@ -85,11 +91,153 @@ export class CatalogosComponent implements OnInit {
                   }
                 });
               }else{
-                console.log(res);
+                console.log("No hay Categorias Nuevas");
               } 
             });
-            
-        });
+          });
+
+        //// CARGA DE MARCAS
+          const catalogoMarcas = [...new Set(res.lstCatalogos.map(item => item.marca))];
+          catalogoMarcas.forEach((value) =>{
+            const marca : MarcasEntity ={
+              id: "",
+              marca: value,
+              etiquetas: "",
+              url_image: ""
+            }   
+            this.httpService.obtenerMarcasNombre(marca).subscribe(res =>{
+              if (res.codigoError != "OK") {
+                const marcanew : MarcasEntity ={
+                  id: "",
+                  marca: value,
+                  etiquetas: value,
+                  url_image: ""
+                }  
+                //console.log(marcanew)
+                this.httpService.agregarMarca(marcanew).subscribe(res =>{
+                  if (res.codigoError == "OK") {
+                    console.log("carga de Marcas con exito ")
+                  }else{
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Ha ocurrido un error en la crecacion de las Marcas.',
+                      text: res.descripcionError,
+                      showConfirmButton: false,
+                    });
+                  }
+                });
+              }else{
+                console.log("No hay Marcas Nuevas");
+              } 
+            });  
+          });
+
+          ///CARGA DE ATRIBUTOS
+          const catalogoAtributos = [...new Set(res.lstCatalogos.map(item => item.caracteristica))];
+          catalogoAtributos.forEach((value) =>{
+            const caracteristica : AtributosEntity ={
+              id: "",
+              atributo: value,
+              etiquetas: ""
+            }   
+            this.httpService.obtenerAtributoNombre(caracteristica).subscribe(res =>{
+              if (res.codigoError != "OK") {
+                const caracteristicanew : AtributosEntity ={
+                  id: "",
+                  atributo: value,
+                  etiquetas: value
+                }   
+               // console.log(caracteristicanew)
+                
+                this.httpService.agregarAtributo(caracteristicanew).subscribe(res =>{
+                  if (res.codigoError == "OK") {
+                    console.log("carga de Caracteristicas con exito ")
+                  }else{
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Ha ocurrido un error en la crecacion de las Caracteristicas.',
+                      text: res.descripcionError,
+                      showConfirmButton: false,
+                    });
+                  }
+                });
+              }else{
+                console.log("No hay Caracteristicas Nuevas");
+              } 
+            });  
+          });
+
+         ///CARGA DE COLORES
+          const catalogoColores = [...new Set(res.lstCatalogos.map(item => item.color))];
+          catalogoColores.forEach((value) =>{
+            const color : ColorsEntity ={
+              id: "",
+              color: value,
+              cod_sap: "",
+              etiquetas: ""
+            }   
+            this.httpService.obtenerColoresNombre(color).subscribe(res =>{
+              if (res.codigoError != "OK") {
+                const colornew : ColorsEntity ={
+                  id: "",
+                  color: value,
+                  cod_sap: value,
+                  etiquetas: value
+                }   
+                //console.log(colornew)
+                this.httpService.agregarColor(colornew).subscribe(res =>{
+                  if (res.codigoError == "OK") {
+                    console.log("carga de Colores con exito ")
+                  }else{
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Ha ocurrido un error en la crecacion de las Colores.',
+                      text: res.descripcionError,
+                      showConfirmButton: false,
+                    });
+                  }
+                });
+              }else{
+                console.log("No hay Colores Nuevos");
+              } 
+            });  
+          });
+
+         ///CARGA DE GENEROS
+         const catalogoGeneros = [...new Set(res.lstCatalogos.map(item => item.genero))];
+         catalogoGeneros.forEach((value) =>{
+            const genero : GenerosEntity ={
+              id: "",
+              genero: value,
+              etiquetas: ""
+            }   
+           this.httpService.obtenerGenerosNombre(genero).subscribe(res =>{
+             if (res.codigoError != "OK") {
+                const generonew : GenerosEntity ={
+                id: "",
+                genero: value,
+                etiquetas: ""
+                } 
+               //console.log(colornew)
+               this.httpService.agregarGenero(generonew).subscribe(res =>{
+                 if (res.codigoError == "OK") {
+                   console.log("carga de Generos con exito ")
+                 }else{
+                   Swal.fire({
+                     icon: 'error',
+                     title: 'Ha ocurrido un error en la crecacion de los Generos.',
+                     text: res.descripcionError,
+                     showConfirmButton: false,
+                   });
+                 }
+               });
+             }else{
+               console.log("No hay Generos Nuevos");
+             } 
+           });  
+         });
+
+
       }
     })
   }
--- src/app/services/catalogos.service.ts
+++ src/app/services/catalogos.service.ts
@@ -2,8 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Atributos, AtributosEntity } from '../models/atributos';
 import { Catalogos, CatalogosEntity } from '../models/catalogos';
 import { Categorias, CategoriasEntity } from '../models/categorias';
+import { Colors, ColorsEntity } from '../models/colors';
+import { Generos, GenerosEntity } from '../models/generos';
+import { Marcas, MarcasEntity } from '../models/marcas';
 
 
 const initGruop: CatalogosEntity = {
@@ -48,10 +52,45 @@ export class CatalogosService {
     return this.http.get<Catalogos>(`${environment.apiUrl}catalogos/ObtenerCatalogos`);
   }
   
+  //FUNCIONES OBTENER  Y AGREGAR CATEGORIAS//
   obtenerCategoriasNombre(categoria: CategoriasEntity): Observable<Categorias> {
     return this.http.post<Categorias>(`${environment.apiUrl}categorias/ObtenerCategoriaNombre`, categoria );
   }
   agregarCategoria(categoria: CategoriasEntity): Observable<Categorias> {
     return this.http.post<Categorias>(`${environment.apiUrl}categorias/InsertarCategorias`, categoria);
+  }
+
+  //FUNCIONES OBTENER  Y AGREGAR MARCAS//
+  obtenerMarcasNombre(marca: MarcasEntity): Observable<Marcas> {
+    return this.http.post<Marcas>(`${environment.apiUrl}marcas/ObtenerMarcaNombre`, marca);
+  }
+  agregarMarca(marca: MarcasEntity): Observable<Marcas> {
+    return this.http.post<Marcas>(`${environment.apiUrl}marcas/InsertarMarcas`, marca);
+  }
+
+  //FUNCIONES OBTENER  Y AGREGAR COLORES//
+  obtenerColoresNombre(color: ColorsEntity): Observable<Colors> {
+    return this.http.post<Colors>(`${environment.apiUrl}colores/ObtenerColoresNombre`, color);
+  }
+
+  agregarColor(color: ColorsEntity): Observable<Colors> {
+    return this.http.post<Colors>(`${environment.apiUrl}colores/InsertarColores`, color);
+  }
+
+  ///FUNCIONES OBTENER Y CARGAR ATRIBUTOS
+  obtenerAtributoNombre(atributo: AtributosEntity): Observable<Atributos> {
+    return this.http.post<Atributos>(`${environment.apiUrl}atributos/ObtenerAtributosNombre`, atributo);
+  }
+  agregarAtributo(atributo: AtributosEntity): Observable<Atributos> {
+    return this.http.post<Atributos>(`${environment.apiUrl}atributos/InsertarAtributos`, atributo);
+  }
+
+  ///FUNCIONES OBTENER Y CARGAR GENEROS///
+  obtenerGenerosNombre(genero: GenerosEntity): Observable<Generos> {
+    return this.http.post<Generos>(`${environment.apiUrl}generos/ObtenerGenerosNombre`, genero);
+  }
+
+  agregarGenero(genero: GenerosEntity): Observable<Generos> {
+    return this.http.post<Generos>(`${environment.apiUrl}generos/InsertarGeneros`, genero);
   }
 }
