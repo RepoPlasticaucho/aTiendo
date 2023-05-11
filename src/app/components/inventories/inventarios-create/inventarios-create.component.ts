003ac04f4600f4fa6582810886aa9705e9ce5604
DanielMontero123 <eduardomontero123@hotmail.com>
2023-05-10 21:13:10 -0500

    Avance Prod. Cl.

--- src/app/components/inventories/inventarios-create/inventarios-create.component.ts
+++ src/app/components/inventories/inventarios-create/inventarios-create.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { faSave, faList, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { ModeloProductosEntity } from 'src/app/models/modeloproductos';
+import { ModelosEntity } from 'src/app/models/modelos';
+import { ModeloproductosService } from 'src/app/services/modeloproductos.service';
+import { ModelosService } from 'src/app/services/modelos.service';
 import Swal from 'sweetalert2';
-
+import { CategoriasEntity } from 'src/app/models/categorias';
+import { CategoriasService } from 'src/app/services/categorias.service';
+import { LineasEntity } from 'src/app/models/lineas';
+import { LineasService } from 'src/app/services/lineas.service';
 @Component({
   selector: 'app-inventarios-create',
   templateUrl: './inventarios-create.component.html',
@@ -19,14 +26,157 @@ export class InventariosCreateComponent implements OnInit {
   inventarioForm = new FormGroup({
     categoria: new FormControl('', Validators.required),
     linea: new FormControl('',),
-    marca_id: new FormControl('0'),
-    marca: new FormControl('0', Validators.required),
+    modelo: new FormControl('0'),
+    modelo_producto: new FormControl('0'),
+    etiquetas: new FormControl('0', Validators.required),
     stock: new FormControl('', [Validators.required]),
   });
 
-  constructor() { }
+  lstCategorias: CategoriasEntity[] = [];
+  selectCategoria: boolean = false;
+
+  lstLineas: LineasEntity[] = [];
+  selectLinea: boolean = false;
+
+  lstModelos: ModelosEntity[] = [];
+  lstModelos2: ModelosEntity[] = [];
+  selectModelo: boolean = false;
+
+  constructor(
+    private readonly httpServiceModelos: ModelosService,
+    private readonly httpService: ModeloproductosService,
+    private readonly httpServiceCategorias: CategoriasService,
+    private readonly httpServiceLineas: LineasService,
+    private router: Router) { }
 
   ngOnInit(): void {
+
+    this.httpServiceCategorias.obtenerCategorias().subscribe(res => {
+      if (res.codigoError != "OK") {
+        Swal.fire({
+          icon: 'error',
+          title: 'No se pudo obtener la Sociedad.',
+          text: res.descripcionError,
+          showConfirmButton: false,
+        });
+      } else {
+        this.lstCategorias = res.lstCategorias;
+      }
+    });
+  }
+
+  visualizarProductos() {
+    this.router.navigate([
+      '/navegation-cl',
+      { outlets: { contentAdmin: ['inventarios-almacen'] } },
+    ]);
+  }
+
+  changeGroup1(e: any) {
+
+    if (e.target.value == 0) {
+      this.selectCategoria = true;
+      this.lstLineas = [];
+      this.lstModelos = [];
+    } else {
+      this.selectCategoria = false;
+    }
+    if (e.target.value == null || undefined) {
+      this.lstLineas = [];
+      this.lstModelos = [];
+    } else {
+      const categoria: CategoriasEntity = {
+        id: '',
+        categoria: e.target.value,
+        cod_sap: '',
+        etiquetas: '',
+        almacen_id : '',
+      }
+      this.httpServiceLineas.obtenerLineasCategoriaAdm(categoria).subscribe(res => {
+        console.log(res);
+        if (res.codigoError != "OK") {
+          Swal.fire({
+            icon: 'error',
+            title: 'No se pudo obtener las líneas.',
+            text: res.descripcionError,
+            showConfirmButton: false,
+          });
+          this.lstLineas = [];
+          this.lstModelos = [];
+        } else {
+          this.lstLineas = res.lstLineas;
+        }
+      })
+    }   
+  }
+  changeGroup2(e: any) {
+
+    if (e.target.value == 0) {
+      this.selectLinea = true;
+      this.lstModelos = [];
+    } else {
+      this.selectLinea = false;
+    }
+    if (e.target.value == null || undefined) {
+      this.lstModelos = [];
+    } else {
+      const linea: LineasEntity = {
+        id: '',
+        categoria_id: '',
+        categoria_nombre: '',
+        linea: e.target.value,
+        etiquetas: '',
+        cod_sap: '',
+        almacen_id: ''
+      }
+      this.httpServiceModelos.obtenerModelosLineasAdm(linea).subscribe(res => {
+        if (res.codigoError != "OK") {
+          Swal.fire({
+            icon: 'error',
+            title: 'No se pudo obtener las líneas.',
+            text: res.descripcionError,
+            showConfirmButton: false,
+          });
+          this.lstModelos = [];
+        } else {
+          this.lstModelos = res.lstModelos;
+        }
+      })
+    }
+  }
+
+  changeGroup3(modelo: any): void {
+    if (modelo.target.value == 0) {
+      this.selectModelo = true;
+    } else {
+      this.selectModelo = false;
+
+      // Obtener ID del modelo
+      //this.selectedModeloProducto = modelo.target.value;
+      const modelonew: ModelosEntity = {
+        id: '',
+        linea_id: '',
+        almacen_id: '',
+        linea_nombre: '',      
+        modelo: modelo.target.value,
+        etiquetas: '',
+        cod_sap: ''
+      }
+      this.httpServiceModelos.obtenerModelosNombre(modelonew).subscribe(res => {
+        if (res.codigoError != "OK") {
+          Swal.fire({
+            icon: 'error',
+            title: 'No se pudo obtener la Sociedad.',
+            text: res.descripcionError,
+            showConfirmButton: false,
+          });
+        } else {
+          this.lstModelos2 = res.lstModelos;
+        }
+      })
+
+      //this.warehousesForm.get("sociedad")?.setValue(sociedad.target.value);
+    }
   }
 
 }
